f872d575da7c162eb04ad22f1f80c13bbcf7139d
Giovanni Collazo <hello@gcollazo.com>
2015-07-30 10:43:19 -0400

    Remove service, move code to x-app

--- app/components/x-app.js
+++ app/components/x-app.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
+import { createRedux, bindActionCreators } from '../redux/index';
+import CounterActions from '../actions/CounterActions';
+import CounterStore from '../stores/counter';
+
 
 function select(state) {
   return { counter: state.counter };
@@ -8,18 +12,22 @@ export default Ember.Component.extend({
   init() {
     this._super(...arguments);
 
-    var redux = this.get('attrs.redux').value;
-    this.set('redux', redux);
+    const stores = {
+      counter: CounterStore
+    };
+
+    const redux = createRedux(stores);
+    const actions = bindActionCreators(CounterActions, redux.dispatch);
 
     // set actions
-    this.set('actions', redux.actions);
+    this.set('actions', actions);
 
     // set initial state
-    this.setProperties(select(redux.instance.getState()));
+    this.setProperties(select(redux.getState()));
 
     // subscribe to change events
-    redux.instance.subscribe(() => {
-      var state = select(redux.instance.getState());
+    redux.subscribe(() => {
+      var state = select(redux.getState());
       this.setProperties(state);
     });
   }
